cf00ca65de009e6e6cc129377be4055b3a6642f7
Trigex <trigexss@gmail.com>
2019-08-05 16:55:43 -0700

    added jint.commonjs fork, moved some stuff around

--- AlphaNET.Framework.Standard/Client/Computer.cs
+++ AlphaNET.Framework.Standard/Client/Computer.cs
@@ -19,7 +19,7 @@ namespace AlphaNET.Framework.Standard.Client
             _console = new Console();
             _interpreter = new JSInterpreter(_filesystem, _console);
             _console.WriteLine("Compiling kernel...");
-            _interpreter.InitAPI(_interpreter.CompilerProxy.CompileTypescript(new string[] { System.IO.File.ReadAllText("OS/src/kernel/kernel.ts"), System.IO.File.ReadAllText("OS/src/kernel/minimist.js") }));
+            _interpreter.InitAPI(_interpreter.CompilerProxy.CompileTypescript(new string[] { IOUtils.ReadManifestData<Computer>("kernel.ts"), IOUtils.ReadManifestData<Computer>("minimist.js") }));
             InstallOS();
         }
 
@@ -36,14 +36,14 @@ namespace AlphaNET.Framework.Standard.Client
         private void InstallOS()
         {
             _console.WriteLine("Compiling and installing OS files...");
-            var initProgram = System.IO.File.ReadAllText("OS/src/init.ts");
-            var shellProgram = System.IO.File.ReadAllText("OS/src/shell.ts");
-            var lsProgram = System.IO.File.ReadAllText("OS/src/ls.ts");
-            var catProgram = System.IO.File.ReadAllText("OS/src/cat.ts");
-            var cdProgram = System.IO.File.ReadAllText("OS/src/cd.ts");
+            var initProgram = IOUtils.ReadManifestData<Computer>("init.ts");
+            var shellProgram = IOUtils.ReadManifestData<Computer>("shell.ts");
+            var lsProgram = IOUtils.ReadManifestData<Computer>("ls.ts");
+            var catProgram = IOUtils.ReadManifestData<Computer>("cat.ts");
+            var cdProgram = IOUtils.ReadManifestData<Computer>("cd.ts");
 
             var bin = (Directory)_filesystem.GetFilesystemObjectsByTitle("bin")[0];
-            _filesystem.AddFilesystemObject(new File("kernel.js", bin, IOUtils.GenerateID(), true, Encoding.UTF8.GetBytes(_interpreter.CompilerProxy.CompileTypescript(System.IO.File.ReadAllText("OS/src/kernel/kernel.ts")))), bin);
+            _filesystem.AddFilesystemObject(new File("kernel.js", bin, IOUtils.GenerateID(), true, Encoding.UTF8.GetBytes(_interpreter.CompilerProxy.CompileTypescript(IOUtils.ReadManifestData<Computer>("kernel.ts")))), bin);
             _filesystem.AddFilesystemObject(new File("init.js", bin, IOUtils.GenerateID(), true, Encoding.UTF8.GetBytes(_interpreter.CompilerProxy.CompileTypescript(initProgram))), bin);
             _filesystem.AddFilesystemObject(new File("shell.js", bin, IOUtils.GenerateID(), true, Encoding.UTF8.GetBytes(_interpreter.CompilerProxy.CompileTypescript(shellProgram))), bin);
             _filesystem.AddFilesystemObject(new File("ls.js", bin, IOUtils.GenerateID(), true, Encoding.UTF8.GetBytes(_interpreter.CompilerProxy.CompileTypescript(lsProgram))), bin);
